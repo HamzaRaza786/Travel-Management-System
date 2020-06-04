e75df1b4e40a660703f9bbe9d55cdf41e0a74c02
Syed Muhammad Hamza Raza <49732070+HamzaRaza786@users.noreply.github.com>
2020-06-04 08:33:37 +0500

    Add files via upload

---
+++ TravelAgency2/src/cFlight.java
@@ -0,0 +1,14 @@
+class cFlight {
+    public double Nigeria = 560;
+    public double Canada_T = 430;
+    public double Canada_O = 460;
+    public double USA_D = 550;
+    public double Kenya = 890;
+    public double Norway = 210;
+    public double Ghana = 570;
+    public double SAfrica = 920;
+    public double UK_L = 300;
+    public double UK_W = 120;
+    public double France = 230;
+    public double Ireland = 260;
+}
