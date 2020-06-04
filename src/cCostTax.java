350c0e62a99656fba79926ae5ae1370222ffc7a9
Syed Muhammad Hamza Raza <49732070+HamzaRaza786@users.noreply.github.com>
2020-06-04 08:35:56 +0500

    Delete cCostTax.java

--- src/cCostTax.java
+++
@@ -1,9 +0,0 @@
-class cCostTax {
-    public double mcTax = 0.90;
-    
-    public double cFindTax(double cAmount)
-    {
-        double FindTax = cAmount - (cAmount * mcTax);    
-        return FindTax;
-    }
-}
